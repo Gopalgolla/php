db3dfe79b9a6e3cb760f28d327107a7a447d403f
Gopalgolla <gopal.golla500@gmail.com>
2024-06-13 13:47:56 +0530

    added the php code tasks

---
+++ datatypes.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $phrase ="I want to learn php";
+    $age = 30;
+    $gpa =34.7;
+    $ismale =false;
+
+    echo $age . '<br/>';
+    echo $gpa;
+    ?>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $value = 50;
+    $name = " gopal yadav";
+    $age = "20";
+    $value=($value*8);
+    echo "my name is $name <br>";
+    echo "25*8=",$value;
+    echo "Iam  $age years old <br>";
+    ?>
+</body>
+</html>
